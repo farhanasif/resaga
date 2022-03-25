8e2344a072dd55d4d17b99de5cb1e6a27e71c43a
Farhan Asif <f.asif2010@gmail.com>
2022-03-26 02:51:25 +0600

    gradient

--- src/screens/CategoriesScreen.js
+++ src/screens/CategoriesScreen.js
@@ -7,51 +7,72 @@ import {
     Image 
 } from 'react-native';
 
+import { LinearGradient } from 'expo-linear-gradient';
+
 const CategoriesScreen = props => {
     //console.log(props)
     return <View style={styles.container}>
-        <Text 
-            style={{ 
-                fontFamily: 'SupermercadoOne', 
-                fontSize: 18 
-            }}>
-                This is the Categories Screen!
-        </Text>
-        <View style={{paddingTop: 5}}>
-            <TouchableOpacity 
-                onPress={() => {
-                    props.navigation.navigate({ routeName: 'CategoryMeals' })
-                }}
-                style={{
-                    backgroundColor: "#d4f6f3",
-                    paddingHorizontal: 30,
-                    paddingVertical: 5,
-                    borderRadius: 5,
-                    flexDirection:"row",
-                    alignItems:'center',
-                    justifyContent:'center',
-                    borderWidth: 2,
-                    borderColor: '#c0f2ea'
-                }}
-            >
-                <Text 
-                    style={{ 
-                        fontFamily: 'SupermercadoOne', 
-                        fontSize: 18 
-                    }}>
-                        Go to Meals
-                </Text>    
-            </TouchableOpacity>
-        </View>
+        <LinearGradient
+        colors={['#fff9fb', '#f4ddff']} 
+        start={{
+            x: 0,
+            y: 0
+        }}
+        end={{
+            x: 1,
+            y: 1
+        }}
+        style={styles.box}>
+            <Text 
+                style={{ 
+                    fontFamily: 'SupermercadoOne', 
+                    fontSize: 18 
+                }}>
+                    This is the Categories Screen!
+            </Text>
+            <View style={{paddingTop: 5}}>
+                <TouchableOpacity 
+                    onPress={() => {
+                        props.navigation.navigate({ routeName: 'CategoryMeals' })
+                    }}
+                    style={{
+                        backgroundColor: "#7D00AE",
+                        paddingHorizontal: 44,
+                        paddingVertical: 14,
+                        borderRadius: 8,
+                        flexDirection:"row",
+                        alignItems:'center',
+                        justifyContent:'center',
+                        borderBottomColor: '#000'
+                        
+                    }}
+                >
+                    <Text 
+                        style={{ 
+                            fontFamily: 'SupermercadoOne', 
+                            fontSize: 13,
+                            color: '#FFF' 
+                        }}>
+                            Go to Meals
+                    </Text>    
+                </TouchableOpacity>
+            </View>
+        </LinearGradient>
     </View>
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    box: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%',
     },
 })
 
